f2f3668c1540f04183b0b5ca3d1513aada7a1ae9
nobilis <nikolaev-evgeny@yandex.ru>
2022-11-28 23:22:17 +0300

    2022-11-28/23.20 Добавлено обращение к сервису с одной предопределенной целевой валютой (USD) - ApiService.kt и ApiFactory.kt. В манифесте добавлено разрешение на доступ к интернету. Из MainActivity.kt. успешно протестированы запросы getTopCoinsInfo() и

--- app/src/main/java/com/example/sryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/sryptoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.sryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.sryptoapp.api.ApiFactory
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ERR")
+            .subscribeOn(Schedulers.io())
+            //.observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {
+                    Log.d("TEST_OF_LOADING_DATA", it.toString())
+                },{
+                    it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+                }
+            )
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+
 }
--- app/src/main/java/com/example/sryptoapp/api/ApiService.kt
+++ app/src/main/java/com/example/sryptoapp/api/ApiService.kt
@@ -1,8 +1,8 @@
 package com.example.sryptoapp.api
 
 import com.example.sryptoapp.pojo.CoinInfoListOfData
-//import io.reactivex.Single
-import io.reactivex.rxjava3.core.Single
+import com.example.sryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -15,10 +15,19 @@ interface ApiService {
         @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
     ) : Single<CoinInfoListOfData>
 
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
+    ) : Single<CoinPriceInfoRawData>
+
     companion object {
         private const val QUERY_PARAM_API_KEY = "api_key"
         private const val QUERY_PARAM_LIMIT = "limit"
         private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
 
         private const val CURRENCY = "USD"
     }
