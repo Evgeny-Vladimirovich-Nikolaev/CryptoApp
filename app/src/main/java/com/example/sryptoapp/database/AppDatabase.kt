b1757f2b11e444a690ff6e12657f6a5d9bd86eeb
nobilis <nikolaev-evgeny@yandex.ru>
2022-11-30 23:27:30 +0300

    2022-11-30/23.21 Добавлен абстрактный класс-синглтон AppDatabase. Изменен класс CoinInfo, удалены поля: id: String? fullName: String? imageUrl: String? оставлено одно поле - val name: String?

---
+++ app/src/main/java/com/example/sryptoapp/database/AppDatabase.kt
@@ -0,0 +1,28 @@
+package com.example.sryptoapp.database
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class AppDatabase : RoomDatabase() {
+
+    companion object {
+        private var db: AppDatabase? = null
+        private const val DB_NAME = "main.db"
+        private val LOCK = Any()
+        fun getInstance(context: Context): AppDatabase {
+            synchronized(LOCK) {
+                db?.let { return it }
+                val instance: AppDatabase =
+                    Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        DB_NAME
+                    ).build()
+                db = instance
+                return instance
+            }
+        }
+    }
+
+}
