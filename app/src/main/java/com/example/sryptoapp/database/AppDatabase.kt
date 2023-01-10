431b171f68ef16a9b6bf835d6d67617d8a5006d5
nobilis <nikolaev-evgeny@yandex.ru>
2023-01-10 23:32:12 +0300

    Merge remote-tracking branch 'origin/2022-12-03/23.05_Dependencies' into 2022-12-03/23.05_Dependencies

--- app/src/main/java/com/example/sryptoapp/database/AppDatabase.kt
+++ app/src/main/java/com/example/sryptoapp/database/AppDatabase.kt
@@ -1,9 +1,12 @@
 package com.example.sryptoapp.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.sryptoapp.pojo.CoinPriceInfo
 
+@Database(entities = [CoinPriceInfo::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     companion object {
@@ -25,4 +28,5 @@ abstract class AppDatabase : RoomDatabase() {
         }
     }
 
+    abstract fun coinPriceInfoDao(): CoinPriceInfoDao
 }
